3a288410f4bc664559577d3123662db30bb0a169
BenjaminPasquier <warriorpasquier@gmail.com>
2020-11-11 15:13:27 +0100

    intial

---
+++ src/containers/Offer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Offer = () => {
+  return (
+    <div>
+      Offer
+      <Link to="/">HomePage</Link>
+    </div>
+  );
+};
+
+export default Offer;
